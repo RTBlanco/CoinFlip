411f316f1fe2804f15f0c5b0541c710d793daa73
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-12 18:57:02 -0400

    added ability to flip with the button

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const status = document.getElementById('status');
   const button = document.getElementById('flip-btn');
-  const coin = document.getElementById('coin');
+  const coin = document.getElementById('inner-coin');
 
-  
+  button.addEventListener('click', (e) => {
+    console.log('fliped')
+    coin.className = 'flip'
+  })
 })
