f2981c77e70898364c0b508933e298807f3361e4
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-12 18:50:20 -0400

    saved the elements in js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const status = document.getElementById('status');
+  const button = document.getElementById('flip-btn');
+  const coin = document.getElementById('coin');
+
+  
+})
