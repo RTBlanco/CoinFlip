6758be83c647babcf327277b56ea56bd9b9cedce
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-12 19:09:02 -0400

    added another way to activate the animation

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const status = document.getElementById('status');
   const button = document.getElementById('flip-btn');
   const coin = document.getElementById('inner-coin');
-
+  const flipAnimation = document.querySelector('.flip');
+  
+  flipAnimation.addEventListener('animationend', () => {
+    console.log('animation ended')
+  });
+  
   button.addEventListener('click', (e) => {
-    console.log('fliped')
-    coin.className = 'flip'
+    console.log('fliped');
+    // if (coin.classList.contains('flip')) {
+      //   coin.classList.
+      // }
+    // coin.className = 'flip';
+      
+    flipAnimation.classList.toggle('active');
+
+
+
   })
 })
